65c67a0f71d2a48ad1e9c4f280da8f469db947b8
YaroslV <yaroslav.voznij@gmail.com>
2014-05-22 04:22:48 +0300

    примітивний графічний інтерфейс

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 
     public static void main(String[] args) {
 
-        //launch(args);
-        System.out.print("Hello loozer");
+        launch(args);
+
     }
 }
