2bc9558a79a7c3035038d9bf10cb6e8178d78f92
YaroslV <yaroslav.voznij@gmail.com>
2014-05-28 00:51:58 +0300

    Update VirtualCoin.java

--- src/sample/VirtualCoin.java
+++ src/sample/VirtualCoin.java
@@ -3,9 +3,19 @@ package sample;
 /**
  * Created by Yaroslav on 5/22/2014.
  */
-public class VirtualCoin extends RealCoin {
+ private BufferedImage img = null;
 
     public void GetImage() {
+
+        try {
+            img = ImageIO.read(new File("bar1.jpg"));
+        } catch (IOException e) {
+        }
     }
+
+    public void Paint(Graphics g) {
+        g.drawImage(img, 50, 50, null);
+    }
+
 
 }
