1ad02cd182b51913d3f32195ceff4e884e5d908e
YaroslV <yaroslav.voznij@gmail.com>
2014-05-28 00:46:26 +0300

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -19,7 +19,9 @@ public class Main extends Application {
 
     public static void main(String[] args) {
 
-        //launch(args);
-        System.out.print("Hello loozer");
+        VirtualCoin VCoin = new VirtualCoin();
+        VCoin.GetImage();
+        VCoin.Paint(Graphics g);
+
     }
 }
